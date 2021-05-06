043483f9dc0d376416a282a94d8d389b7fafffa7
Pablo <pablochavezing@gmail.com>
2021-05-05 18:42:00 -0700

    migraciones

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_06_014105) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "url_link"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
