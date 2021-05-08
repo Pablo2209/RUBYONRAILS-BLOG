cec7c0bd956f718e69965564d2f81da5b2549647
Pablo <pablochavezing@gmail.com>
2021-05-08 10:48:26 -0700

    Sensurar palabra del contenido

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
    /validar los campos/ 
     validates :title, :url_link, :content, presence: true
+
+    /Eliminar cierta parabra que no queramos/
+    before_save :replace_word
+
+    def replace_word
+        self.content = self.content.gsub "spoiler", " "
+    end
 end
